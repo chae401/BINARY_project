1da0dbcc8e9b6f2a79dcbf7fa32ca68631f0ee60
Marie00127 <haeree1227@gmail.com>
2023-02-21 23:06:11 +0900

    add image, dataset dose + training code
    add train image, data collection code + training code

--- study/src/main/resources/CV/Hot_Desking_Training.py
+++ study/src/main/resources/CV/Hot_Desking_Training.py
@@ -1,44 +1,68 @@
 import cv2
 import numpy as np
 from PIL import Image
+from glob import glob
 import os
 
-detector = cv2.CascadeClassifier('.\haarcascade_frontalface_default.xml')
+
+path = 'study\src\main\\resources\CV\Face_Dataset\\'
+
+# 그냥 recognizer = cv2.face.LBPHFaceRecognizer_create() 이것만 실행하면
+# 계속 cv2에 face 모듈이 존재하지 않는다고 오류 발생
+# print(dir(cv2.face)) 코드 추가
+# 코드 추가하니까 recognizer = cv2.face.LBPHFaceRecognizer_create() 잘 실행 됨
+dir(cv2.face)
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+
+detector = cv2.CascadeClassifier('study\src\main\\resources\CV\haarcascade_frontalface_default.xml')
+
 
 def getImagesAndLabels(path):
-    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+
+    image_paths = glob(os.path.join(path, '*', '*'))
     # listdir == 해당 디렉토리 내 파일 리스트
     # path + file Name == 경로 list 만들기
 
+
+
+    for i in range(len(image_paths)):
+        print(image_paths[i])
+
+
     faceSamples = []
     ids = []
-    for imagePath in imagePaths: # 각 파일마다
+
+    for image_path in image_paths: # 각 파일마다
         # 흑백 변환
         # 8 bits pixel 이미지로 변환 => 0 ~ 255의 수로 표현 가능한 흑백 이미지 생성
-        PIL_img = Image.open(imagePath).convert('L') # L : 8 bit pixel, bw
-        img_numpy = np.array(PIL_img, 'uint8')
+        print('image_path = ', image_path)
 
-        # user id
-        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        img = Image.open(image_path).convert('L') # L : 8 bit pixel, bw
+        img_numpy = np.array(img, 'uint8')
+
+        print("point1 Pass\n")
+
+        id = int(os.path.split(image_path)[-2].split('\\')[-1])
 
         print(id)
 
         # 학습을 위한 얼굴 샘플
         faces = detector.detectMultiScale(img_numpy)
+
         for(x,y,w,h) in faces:
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
 
     return faceSamples, ids
 
-path = 'dataset'
-# 유사 얼굴 찾아내는 모델
-recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-print('\n Training faces. It will take a few seconds. Wait ...')
+print('\n Training faces. It will take a few seconds.` Wait ...')
+
 faces, ids = getImagesAndLabels(path)
 
-recognizer.train(faces,np.array(ids)) #학습
+recognizer.train(faces, np.array(ids)) #학습
 
-recognizer.write('.\/train/trainer.yml')
-print('\n {0} faces trained. Exiting Program'.format(len(np.unique(ids))))
+recognizer.write('study\src\main\\resources\CV\\train\\trainer.yml')
+
+print(f"\n {len(np.unique(ids))} faces trained. Exiting Program")
